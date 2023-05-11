c92e466201973d906e6cbe6a1cc6e5a836f46a85
Sokka44 <133067118+Sokka44@users.noreply.github.com>
2023-05-11 17:49:46 -0600

    Update Paint.py
    Definimos el Triangulo y Rectangulo dentro del juego

--- Paint.py
+++ Paint.py
@@ -43,12 +43,35 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    width = end.x - start.x
+    height = end.y - start.y
+
+    for _ in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for _ in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 
 def tap(x, y):
